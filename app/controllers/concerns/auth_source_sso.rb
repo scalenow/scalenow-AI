b6b03753740309d05493104c29ced4e2d6ebf1be
AmanVhora <aman.vhora@aspireedge.com>
2025-12-08 17:03:35 +0530

    Update file changes made by client

--- app/controllers/concerns/auth_source_sso.rb
+++ app/controllers/concerns/auth_source_sso.rb
@@ -215,7 +215,7 @@ module AuthSourceSSO
 
   def perform_post_logout(prev_session, previous_user)
     if prev_session[:user_from_auth_header] && header_slo_url.present?
-      redirect_to(header_slo_url, allow_other_host: true)
+      redirect_to(header_slo_url)
     else
       super
     end
